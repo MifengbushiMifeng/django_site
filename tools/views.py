5bcafc937ca416fec8f3e71ed1a04d6754f575aa
Jonathan <zhouronghui30@163.com>
2017-05-18 17:18:22 +0800

    practice django - form

--- tools/views.py
+++ tools/views.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .forms import AddForm
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+
+        if form.is_valid():
+            a = form.cleaned_data['a']
+            b = form.cleaned_data['b']
+            return HttpResponse(str(int(a) + int(b)))
+
+    else:
+        form = AddForm()
+
+    return render(request, 'index.html', {'form': form})
